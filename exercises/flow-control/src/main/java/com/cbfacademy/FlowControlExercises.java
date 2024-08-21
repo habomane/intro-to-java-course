4b7835d21416e4317ea46187ec14968e5ff601f9
omaneh <122321976+omaneh@users.noreply.github.com>
2024-08-20 20:21:25 -0400

    Finished exercises

--- exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
+++ exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
@@ -1,6 +1,7 @@
 package com.cbfacademy;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,14 +15,53 @@ public class FlowControlExercises {
         //  - if the ${element} is divisible by both 3 and 5, it adds “FizzBuzz” to the list
         //  - it adds the element to the list in any other case
         //  - it returns the constructed list
-        throw new RuntimeException("Not implemented");
+        List<String> otherItems = new ArrayList<>();
+        for (Integer number : numbers)
+        {
+            if(number % 3 == 0 && number % 5 == 0) {
+                otherItems.add("FizzBuzz");
+            }
+            else if(number % 3 == 0) { otherItems.add("Fizz");}
+            else if(number % 5 == 0) { otherItems.add("Buzz");}
+            else { otherItems.add(String.valueOf(number));}
+        }
+
+        return otherItems;
     }
 
     public String whichMonth(Integer number) {
         // TODO - Implement this method such that
         //  - it returns the month corresponding to the input ${number}
         //  - if the ${number} is invalid, the method should return "Invalid month number"
-        throw new RuntimeException("Not implemented");
+        switch(number)
+        {
+            case 1:
+            return "January";
+            case 2:
+            return "February";
+            case 3:
+            return "March";
+            case 4:
+            return "April";
+            case 5:
+            return "May";
+            case 6:
+            return "June";
+            case 7:
+            return "July";
+            case 8:
+            return "August";
+            case 9:
+            return "September";
+            case 10:
+            return "October";
+            case 11:
+            return "November";
+            case 12:
+            return "December";
+            default:
+            return "Invalid month number";
+        }
     }
 
     public Map<String, Integer> sumOfOddsAndSumOfEvens() {
@@ -31,14 +71,39 @@ public class FlowControlExercises {
         //  - determines the sum of all the odd numbers in the list
         //  - returns a map with two entries:
         //      {"SumOfEvens", calculatedSumOfEvens}, {"SumOfOdds", calculatedSumOfOdds}
-        throw new RuntimeException("Not implemented");
+        List<Integer> evens = new ArrayList<>();
+        List<Integer> odds = new ArrayList<>();
+        for(int i = 1; i <= 100; i++)
+        {
+            if(i % 2 == 0) 
+            {
+                evens.add(i);
+                continue;
+            }
+
+            odds.add(i);
+        }
+
+        Map<String, Integer> sums = new HashMap<>();
+
+        sums.put("SumOfEvens", evens.stream().mapToInt(Integer::valueOf).sum());
+        sums.put("SumOfOdds", odds.stream().mapToInt(Integer::valueOf).sum());
+        return sums;
+
     }
 
     public List<Integer> reverse(ArrayList<Integer> numbers) {
         // TODO - Implement this method such that
         //  - it takes an array list of integers
         //  - it returns the list in reverse order
-        throw new RuntimeException("Not implemented");
+        List<Integer> reverseNumbers = new ArrayList<>();
+
+        for(int i = numbers.size() - 1; i  >= 0 ; i--)
+        {
+            reverseNumbers.add(numbers.get(i));
+        }
+
+        return reverseNumbers;
     }
 
     public String getName() {
